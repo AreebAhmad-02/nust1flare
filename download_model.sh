29b4c4d3b2f5a8c8162fc7bd669f839b7cf18486
Areeb Ahmad <ahmadareeb3026@gmail.com>
2025-08-11 21:46:09 +0500

    added workflows file to free space

--- download_model.sh
+++ download_model.sh
@@ -4,13 +4,13 @@ echo "Downloading model from Hugging Face..."
 if [ ! -f "/workspace/results/baseline/model-00004-of-00004.safetensors" ]; then
     echo "[INFO] Model not found. Downloading from Hugging Face..."
     
-    $MODEL_DIR="/workspace/results/baseline"
+    MODEL_DIR="/workspace/results/baseline"
     HF_REPO="Areeb-02/M3D-LaMed-Phi-3-4B-finetuned"
 
     huggingface-cli download $HF_REPO --local-dir $MODEL_DIR
 
     echo "[INFO] Model downloaded to $MODEL_DIR"
-    
+
 else
     echo "[INFO] Model already present, skipping download."
 fi
